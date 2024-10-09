84ac103989dbcdf0314b9027d35301b88cecc670
Sergio Peña <spena@irontec.com>
2024-10-09 09:49:22 +0200

    Update fizz-buzz.js

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,16 +1,17 @@
-function fizzBuzz() {
-    // create the fizzbuzz logic here
-    for (let i = 1; i <= 100; i++) {
+function fizzBuzz(n) {
+    let result = [];
+    for (let i = 1; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz");
+            result.push("FizzBuzz");
         } else if (i % 3 === 0) {
-            console.log("Fizz");
+            result.push("Fizz");
         } else if (i % 5 === 0) {
-            console.log("Buzz");
+            result.push("Buzz");
         } else {
-            console.log(i);
+            result.push(i.toString());
         }
     }
+    return result;
 }
 
-fizzBuzz();
+console.log(fizzBuzz(15));
